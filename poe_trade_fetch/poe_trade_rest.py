4b25273ca2850ab10f214f9257ce6bd80fe44e64
Ramonnn <29675067+Ramonnn@users.noreply.github.com>
2024-02-20 21:19:55 +0100

    Refactor into OOB style

--- poe_trade_fetch/poe_trade_rest.py
+++ poe_trade_fetch/poe_trade_rest.py
@@ -2,42 +2,54 @@ import requests
 import pandas as pd
 import time
 
-def fetch_listings(trade_url, payload, header):
-    df = pd.DataFrame()
-    try:
-        r = requests.post(trade_url, headers=header, json=payload)
-        if r.status_code == 200:
+class ListingFetcher:
+    def __init__(self, trade_url, header, payload) -> None:
+        self.trade_url = trade_url
+        self.header = header
+        self.payload = payload
+
+    def fetch_listing(self):
+        try:
+            r = requests.post(self.trade_url, headers=self.header, json=self.payload.query)
+            r.raise_for_status()
             result = r.json().get('result', [])[:10]
             result_id = r.json().get('id', '')
             text_result = ','.join(result)
             fetch_url = f'https://www.pathofexile.com/api/trade/fetch/{text_result}?query={result_id}'
-            listings = requests.get(fetch_url, headers=header)
+            listings = requests.get(fetch_url, headers=self.header)
+            r.raise_for_status()
             result_list = listings.json().get('result', [])
+            return result_list
+        except requests.RequestException as e:
+            print("Error: ", e)
+            return []
 
+    def extract_properties(self, item_properties):
+        item_level, item_quality = None, None
+        for prop in item_properties:
+            if prop['name'] == 'Level':
+                item_level = prop['values'][0][0]
+            elif prop['name'] == 'Quality':
+                item_quality = prop['values'][0][0]
+        return item_level, item_quality
+
+    def extract_gem_experience(self, additional_properties):
+        for prop in additional_properties:
+            if prop.get('name') == 'Experience':
+                return prop.get('values', [])[0][0]
+        return None
+
+    def extract_data(self):
             extracted_data = []
 
-            for item in result_list:
+            for item in self.fetch_listing():
                 listing = item.get('listing', {})
                 item_info = item.get('item', {})
 
                 item_name = item_info.get('typeLine', '')
                 item_properties = item_info.get('properties', [])
-
-                item_level = None
-                item_quality = None
-                for prop in item_properties:
-                    if prop['name'] == 'Level':
-                        item_level = prop['values'][0][0]
-                    if prop['name'] == 'Quality':
-                        item_quality = prop['values'][0][0]
-
-                gem_experience = None
-                additional_properties = item_info.get('additionalProperties', [])
-                if additional_properties:
-                    for prop in additional_properties:
-                        if prop.get('name') == 'Experience':
-                            gem_experience = prop.get('values', [])[0][0]
-                            break
+                item_level, item_quality = self.extract_properties(item_properties)
+                gem_experience = self.extract_gem_experience(item_info.get('additionalProperties', []))
 
                 indexed = listing.get('indexed', '')
                 stash_name = listing.get('stash',{}).get('name', '')
@@ -61,15 +73,13 @@ def fetch_listings(trade_url, payload, header):
                 
                 extracted_data.append(row)
 
-            df = pd.DataFrame(extracted_data)
+            return extracted_data
 
+    def save_data(self):
+        df = pd.DataFrame(self.extract_data())
+        item_words=self.payload.item_type.split()
+        df.to_csv(f"{'_'.join(item_words)}.csv")
 
-        else:
-            print(f"Error: Failed to fetch listings. Status Code: {r.status_code}")
-    except Exception as e:
-        print("Error:", e)
-
-    return df
 
 class Payload:
     def __init__(self, status="online", item_type="", league="Affliction", max_quality=None, sort_by="price", sort_order="asc") -> None:
@@ -103,12 +113,12 @@ class Payload:
         }
 
 # payload = Payload(item_type="Awakened Spell Echo Support")
-def fetch_all_listings(item_names, header, trade_url, max_quality, sort_by):
+
+def fetch_all_listings(item_names, header, trade_url):
     for item in item_names:
-        payload = Payload(item_type=item, max_quality=max_quality, sort_by=sort_by)
-        item_words = item.split()
-        listing = fetch_listings(trade_url, payload.query, header)
-        listing.to_csv(f"{'_'.join(item_words)}.csv")
+        payload = Payload(item_type=item)
+        fetcher = ListingFetcher(trade_url, header, payload)
+        fetcher.save_data()
         time.sleep(10)
 
 # poe_trade_df = poe_trade_rest.fetch_listings(trade_url, payload.query, header)

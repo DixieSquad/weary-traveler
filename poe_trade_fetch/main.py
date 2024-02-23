2bbafbb2d9062f3876524038f5d827016e92abe9
Ramonnn <29675067+Ramonnn@users.noreply.github.com>
2024-02-23 20:57:42 +0100

    Calculate profit per gem
    Added ipynd_checkpoints and poe_ninja.csv to gitignore

--- poe_trade_fetch/main.py
+++ poe_trade_fetch/main.py
@@ -17,8 +17,6 @@ poe_ninja_url = "https://poe.ninja/economy/skill-gems"
 
 poe_trade_rest.fetch_all_listings(gems, properties, header, trade_url)
 
-# poe_ninja_df = scraper.fetch_data(poe_ninja_url)
-# poe_trade_df = poe_trade_rest.fetch_listings(trade_url, payload.query, header)
+poe_ninja_df = scraper.fetch_data(poe_ninja_url)
 
-# poe_ninja_df.to_csv('poe_ninja_data.csv')
-# poe_trade_df.to_csv('poe_trade_data.csv')
+poe_ninja_df.to_csv("poe_ninja_data.csv")

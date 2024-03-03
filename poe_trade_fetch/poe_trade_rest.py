ebb4626f4493796fbede04ac823a057d8735c9e5
NimbusNiles <29178202+NimbusNiles@users.noreply.github.com>
2024-03-03 22:07:22 +0100

    Fix checking for empty return data

--- poe_trade_fetch/poe_trade_rest.py
+++ poe_trade_fetch/poe_trade_rest.py
@@ -201,25 +201,25 @@ class BuySellEntry:
 
         buy_sell_dict = {}
 
-        buy_data = self.buy_listings.extract_data()
-        sell_data = self.sell_listings.extract_data()
+        buy_data = pd.DataFrame(self.buy_listings.extract_data())
+        sell_data = pd.DataFrame(self.sell_listings.extract_data())
         buy_sell_dict["Item Name"] = self.buy_listings.payload.item_type
 
-        if not buy_data:
+        if buy_data.empty:
             buy_sell_dict["Buy"] = None
         else:
-            buy_data = pd.DataFrame(buy_data).astype({"Price Amount": float})
+            buy_data = buy_data.astype({"Price Amount": float})
             buy_data = self.convert_chaos_to_divine(buy_data)
             buy_sell_dict["Buy"] = round(buy_data["Price Amount"].mean(), 1)
 
-        if not sell_data:
+        if sell_data.empty:
             buy_sell_dict["Sell"] = None
         else:
-            sell_data = pd.DataFrame(sell_data).astype({"Price Amount": float})
+            sell_data = sell_data.astype({"Price Amount": float})
             sell_data = self.convert_chaos_to_divine(sell_data)
             buy_sell_dict["Sell"] = round(sell_data["Price Amount"].mean(), 1)
 
-        if not buy_data or not sell_data:
+        if buy_data.empty or sell_data.empty:
             buy_sell_dict["Profit"] = None
         else:
             buy_sell_dict["Profit"] = round(

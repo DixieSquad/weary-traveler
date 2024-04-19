0aee183087616abe4a41a2c995b489543f8721c7
NimbusNiles <29178202+NimbusNiles@users.noreply.github.com>
2024-04-19 16:52:11 +0200

    Add get oldest entry to DataHandler, including tests

--- tests/test_DataHandler.py
+++ tests/test_DataHandler.py
@@ -1,10 +1,28 @@
 import pytest
 from poe_trade_fetch.poe_trade_rest import DataHandler, ItemEntry
-from datetime import datetime
+from datetime import datetime, timedelta
+import os
+import shutil
+
+
+@pytest.fixture()
+def prep_and_clean_data():
+    data_folder = "data"
+    item_folder = "data/item_entries"
+    profit_folder = "data/profit_strats"
+    filename = "awakened_gems.json"
+
+    print("Setting up test data folders")
+    os.mkdir(data_folder)
+    os.mkdir(item_folder)
+    os.mkdir(profit_folder)
+    yield
+    print("Removing test data folders")
+    shutil.rmtree("data")
 
 
 @pytest.fixture
-def item_entry1() -> ItemEntry:
+def item_entry() -> ItemEntry:
     item_entry = ItemEntry(
         id=1,
         item_name="test1",
@@ -18,30 +36,48 @@ def item_entry1() -> ItemEntry:
 
 
 @pytest.fixture
-def item_entry2() -> ItemEntry:
-    item_entry = ItemEntry(
-        id=1,
-        item_name="test2",
-        modifiers={"mod1": 2},
-        url="test_url",
-        value=12.3,
-        number_listed=12,
-        updated_at=datetime.now(),
-    )
-    return item_entry
+def datahandler(prep_and_clean_data) -> DataHandler:
+    return DataHandler()
 
 
-def test_saving_and_loading_json(item_entry1) -> None:
-    dataHandler = DataHandler()
-    item_entry = item_entry1
-    dataHandler.write_item_entry(item_entry=item_entry)
-    item_entries = dataHandler.read_all_item_entries()
-    assert item_entry1 in item_entries
+def test_saving_and_loading_json(item_entry, datahandler) -> None:
+    item_entry = item_entry
+    datahandler.write_item_entry(item_entry=item_entry)
+    item_entries = datahandler.read_all_item_entries()
+    assert item_entry in item_entries
 
 
-def test_item_entry_equals(item_entry1) -> None:
-    assert item_entry1 == item_entry1
+def test_get_oldest_entry_last(item_entry, datahandler) -> None:
+    item_entry1 = ItemEntry(**item_entry.__dict__)
+    item_entry2 = ItemEntry(**item_entry.__dict__)
+    item_entry2.item_name = "test2"
+    item_entry2.updated_at = datetime.now() - timedelta(days=1)
+    datahandler.write_item_entry(item_entry1)
+    datahandler.write_item_entry(item_entry2)
+
+    item = datahandler.get_oldest_entry()
+    assert item == item_entry2 and item != item_entry1
 
 
-def test_item_entry_not_equals(item_entry1, item_entry2) -> None:
-    assert item_entry1 != item_entry2
+def test_get_oldest_entry_first(item_entry, datahandler) -> None:
+    item_entry1 = ItemEntry(**item_entry.__dict__)
+    item_entry2 = ItemEntry(**item_entry.__dict__)
+    item_entry1.item_name = "test2"
+    item_entry1.updated_at = datetime.now() - timedelta(days=1)
+    datahandler.write_item_entry(item_entry1)
+    datahandler.write_item_entry(item_entry2)
+
+    item = datahandler.get_oldest_entry()
+    assert item == item_entry1 and item != item_entry2
+
+
+def test_item_entry_equals(item_entry) -> None:
+    assert item_entry == item_entry
+
+
+def test_item_entry_not_equals(item_entry) -> None:
+    item_entry2 = ItemEntry(**item_entry.__dict__)
+    item_entry2.item_name = "test2"
+    item_entry2.modifiers = {"mod1": 2}
+    print(item_entry)
+    assert item_entry != item_entry2
